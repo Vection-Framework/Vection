1d92191075a3982081ff9bb1810694f489991ff0
David <aleqz.lung@gmail.com>
2019-08-10 19:27:26 +0200

    [Fix/Event] AnnotationMapper: Use priority of 0 if not set (avoid index notice)

--- src/Component/Event/EventAnnotationMapper.php
+++ src/Component/Event/EventAnnotationMapper.php
@@ -1,10 +1,10 @@
 <?php declare(strict_types=1);
 
 /**
- * This file is part of the Vection project.
- * Visit project at https://www.vection.de
+ * This file is part of the Vection-Framework project.
+ * Visit project at https://github.com/Vection-Framework/Vection
  *
- * (c) Vection <project@vection.de>
+ * (c) David M. Lung <vection@davidlung.de>
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
@@ -186,7 +186,7 @@ class EventAnnotationMapper implements CacheAwareInterface
 
                     $mapping[$subscription['event']][] = [
                         [ $className, $subscription['method'] ],
-                        $subscription['priority']
+                        $subscription['priority'] ?? 0
                     ];
 
                 }
