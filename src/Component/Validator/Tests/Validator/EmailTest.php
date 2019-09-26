214b46d5a0373fdc138a8247b300885fbff4fb7f
BloodhunterD <vection@bloodhunterd.com>
2019-09-26 19:37:52 +0200

    extended email validator test

--- src/Component/Validator/Tests/Validator/EmailTest.php
+++ src/Component/Validator/Tests/Validator/EmailTest.php
@@ -45,20 +45,23 @@ class EmailTest extends TestCase
      */
     public function provideValidValues(): array
     {
-        $domain = 'vection.de';
+        $tld = '.com';
+
+        $domain = 'example';
 
         // Local mail part allows max 64 chars
         $local = bin2hex(random_bytes(32));
 
-        $subDomain = implode('.', array_fill(0, 93, 'a')) . '.com';
+        $subDomain = implode('.', array_fill(0, 119, 'a')) . $tld;
 
         return [
-            [$local . '@' . $domain],
-            ['John.Doe@' . $subDomain],
-            ['!#$%&\'*+-/=?^_`{|}~@' . $domain],
-            ['".John..Doe."@' . $domain],
-            ['"@"@' . $domain],
-            ['"John"."Doe"@' . $domain]
+            [$local . '@' . $domain . $tld],
+            ['John.Doe@' . $subDomain . $tld],
+            ['!#$%&\'*+-/=?^_`{|}~@' . $domain . $tld],
+            ['".John..Doe."@' . $domain . $tld],
+            ['"@"@' . $domain . $tld],
+            ['"John"."Doe"@' . $domain . $tld],
+            ['John.Doe@' . $domain . '.travelersinsurance'],
         ];
     }
 
@@ -68,23 +71,29 @@ class EmailTest extends TestCase
      */
     public function provideInvalidValues(): array
     {
-        $domain = 'vection.de';
+        $tld = '.com';
+
+        $domain = 'example';
 
         // Local mail part allows max 64 chars
         $local = bin2hex(random_bytes(32));
 
+        $subDomain = implode('.', array_fill(0, 126, 'a')) . $tld;
+
         return [
-            ['John.Doe-' . $domain],
-            ['.John.Doe.' . $domain],
-            ['.John.Doe@' . $domain],
-            ['John.Doe.@' . $domain],
-            ['John..Doe@' . $domain],
-            ['John@Doe@' . $domain],
-            ['a"b(c)d,e:f;g<h>i[j\k]l@' . $domain],
-            ['John"not"Doe@' . $domain],
-            ['John"not\Doe@' . $domain],
-            ['John\ still\"not\\Doe@' . $domain],
-            [$local . 'a@' . $domain]
+            ['John.Doe-' . $domain . $tld],
+            ['.John.Doe.' . $domain . $tld],
+            ['.John.Doe@' . $domain . $tld],
+            ['John.Doe.@' . $domain . $tld],
+            ['John..Doe@' . $domain . $tld],
+            ['John@Doe@' . $domain . $tld],
+            ['a"b(c)d,e:f;g<h>i[j\k]l@' . $domain . $tld],
+            ['John"not"Doe@' . $domain . $tld],
+            ['John"not\Doe@' . $domain . $tld],
+            ['John\ still\"not\\Doe@' . $domain . $tld],
+            [$local . 'a@' . $domain . $tld],
+            ['John.Doe@' . $subDomain . $tld],
+            ['John.Doe@' . $domain . $local]
         ];
     }
 }
