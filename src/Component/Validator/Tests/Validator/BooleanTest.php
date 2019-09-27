88ebe1129bb9e5a36177cf9df25fd5a71e513013
BloodhunterD <vection@bloodhunterd.com>
2019-09-27 21:45:02 +0200

    copyright notes adjusted

--- src/Component/Validator/Tests/Validator/BooleanTest.php
+++ src/Component/Validator/Tests/Validator/BooleanTest.php
@@ -1,12 +1,12 @@
 <?php
 /**
- *  This file is part of the AppsDock project.
- *  Visit project at https://github.com/Vection-Framework/Vection
+ * This file is part of the Vection-Framework project.
+ * Visit project at https://github.com/Vection-Framework/Vection
  *
- *  © BloodhunterD <vection@bloodhunterd.com>
+ * © BloodhunterD <vection@bloodhunterd.com>
  *
- *  For the full copyright and license information, please view the LICENSE
- *  file that was distributed with this source code.
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
  */
 
 namespace Vection\Component\Validator\Tests\Validator;
