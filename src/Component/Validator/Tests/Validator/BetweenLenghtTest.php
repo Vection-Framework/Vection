fcca6aa66f9d05b1115282050ad1a2b03187117e
BloodhunterD <vection@bloodhunterd.com>
2019-10-03 14:45:48 +0200

    added between length validator test

---
+++ src/Component/Validator/Tests/Validator/BetweenLenghtTest.php
@@ -0,0 +1,78 @@
+<?php
+/**
+ * This file is part of the Vection-Framework project.
+ * Visit project at https://github.com/Vection-Framework/Vection
+ *
+ * © BloodhunterD <vection@bloodhunterd.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Vection\Component\Validator\Tests\Validator;
+
+use PHPUnit\Framework\TestCase;
+use Vection\Component\Validator\Validator\BetweenLength;
+
+/**
+ * Class BetweenLenghtTest
+ *
+ * @package Vection\Component\Validator\Tests\Validator
+ */
+class BetweenLenghtTest extends TestCase
+{
+
+    /**
+     * @dataProvider provideValidValues
+     */
+    public function testValidValues($min, $max, $value): void
+    {
+        $this->assertNull((new BetweenLength($min, $max))->validate($value));
+    }
+
+    /**
+     * @dataProvider provideInvalidValues
+     */
+    public function testInvalidValues($min, $max, $value): void
+    {
+        $this->assertNotNull((new BetweenLength($min, $max))->validate($value));
+    }
+
+    /**
+     * @return array
+     * @throws \Exception
+     */
+    public function provideValidValues(): array
+    {
+        $values = [
+          [0, 64, 16],
+          [10, 20, 10],
+          [256, 512, 256]
+        ];
+
+        foreach ($values as $value) {
+            $return[] = [$value[0], $value[1], bin2hex(random_bytes($value[2]))];
+        }
+
+        return $return ?? [];
+    }
+
+    /**
+     * @return array
+     * @throws \Exception
+     */
+    public function provideInvalidValues(): array
+    {
+        $values = [
+            [0, 64, 128],
+            [10, 20, 2],
+            [256, 512, 64]
+        ];
+
+        foreach ($values as $value) {
+            $return[] = [$value[0], $value[1], bin2hex(random_bytes($value[2]))];
+        }
+
+        return $return ?? [];
+    }
+}
