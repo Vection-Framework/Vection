739278ebb240c27af824ee89dade34be0f8a1ad5
BloodhunterD <vection@bloodhunterd.com>
2019-10-06 14:29:27 +0200

    added phone E164 validator

---
+++ src/Component/Validator/Validator/PhoneE164.php
@@ -0,0 +1,39 @@
+<?php declare(strict_types=1);
+
+/**
+ * This file is part of the Vection-Framework project.
+ * Visit project at https://github.com/Vection-Framework/Vection
+ *
+ * © BloodhunterD <vection@bloodhunterd.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Vection\Component\Validator\Validator;
+
+use Vection\Component\Validator\Validator;
+
+/**
+ * Class PhoneE164
+ *
+ * @package Vection\Component\Validator\Validator
+ */
+class PhoneE164 extends Validator
+{
+    /**
+     * @inheritDoc
+     */
+    public function getMessage(): string
+    {
+        return 'Value "{value}" is not a valid E164 formatted phone number.';
+    }
+
+    /**
+     * @inheritDoc
+     */
+    protected function onValidate($value): bool
+    {
+        return (bool) preg_match('/^\+?[1-9]\d{1,14}$/', $value);
+    }
+}
