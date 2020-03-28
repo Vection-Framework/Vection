59fd467bcf5cb477fa6ca6f55bc78bc73ded85e6
BKlemm <bkl3mm@googlemail.com>
2020-03-28 12:58:14 +0100

    [Utility] #CHANGE 'refactor:tests' {moved tests to root Utility folder}

--- src/Component/Utility/Tests/StringTypeTest.php
+++ src/Component/Utility/Tests/StringTypeTest.php
@@ -3,13 +3,13 @@
  * This file is part of the Vection-Framework project.
  * Visit project at https://github.com/Vection-Framework/Vection
  *
- * (c) Vection-Framework <vection@appsdock.de>
+ * (c) Bjoern Klemm <vection@bjoernklemm.de>
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
 
-namespace Vection\Component\Utility\Calendar\Tests;
+namespace Vection\Component\Utility\Tests;
 
 
 use PHPUnit\Framework\TestCase;
