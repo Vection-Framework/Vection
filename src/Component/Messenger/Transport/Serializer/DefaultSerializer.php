8c88d54b41542804e1174a45d6b1e4ed0f869b6d
David <aleqz.lung@gmail.com>
2020-06-13 14:33:05 +0200

    [Messenger] #BUGFIX class:DefaultSerializer Fix unserialization by adding option 'allow_classes'.

--- src/Component/Messenger/Transport/Serializer/DefaultSerializer.php
+++ src/Component/Messenger/Transport/Serializer/DefaultSerializer.php
@@ -1,10 +1,9 @@
 <?php
 
 /**
- * This file is part of the Vection-Framework project.
- * Visit project at https://github.com/Vection-Framework/Vection
+ * This file is part of the Vection package.
  *
- * (c) Vection-Framework <vection@appsdock.de>
+ * (c) David M. Lung <vection@davidlung.de>
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
@@ -38,6 +37,6 @@ class DefaultSerializer implements SerializerInterface
      */
     public function unserialize(string $serial, string $type): object
     {
-        return unserialize($serial, [$type]);
+        return unserialize($serial, ['allowed_classes' => $type]);
     }
 }
