d2ee1bfb3b8f32379ba94bea676e33e15d0ba4dc
David <aleqz.lung@gmail.com>
2020-10-09 16:21:47 +0200

    [Contracts] #EXTEND interface:Messenger.ReceiverInterface Add the "nack" method and adapt comments. The nack can be now used for negative acknowledge with message requeue.

--- src/Contracts/Messenger/Transport/ReceiverInterface.php
+++ src/Contracts/Messenger/Transport/ReceiverInterface.php
@@ -26,21 +26,45 @@ use Vection\Contracts\Messenger\MessageInterface;
 interface ReceiverInterface
 {
     /**
+     * Setup the receiver client with client specific options
+     * provided by the choosed provider.
+     *
      * @param array $options
      */
     public function setup(array $options = []): void;
 
     /**
+     * Returns the next incoming message.
+     *
+     * If there is no message actually send from MQ server,
+     * this method returns null.
+     *
      * @return MessageInterface|null
      */
     public function next(): ?MessageInterface;
 
     /**
+     * Sends an acknolegement flag to the MQ server.
+     *
      * @param MessageInterface $message
      */
     public function ack(MessageInterface $message): void;
 
     /**
+     * Sends an negative acknolegement flag to the MQ server.
+     * NACKed masseges will be requeued by default, use the second
+     * parameter to change this default behavior.
+     *
+     * @param MessageInterface $message
+     * @param bool             $requeue
+     */
+    public function nack(MessageInterface $message, bool $requeue = true): void;
+
+    /**
+     * Rejects the message back to the MQ server.
+     * This method is used to do an explicit drop of the message from queue
+     * to prevent redelivery by the MQ server.
+     *
      * @param MessageInterface $message
      */
     public function reject(MessageInterface $message): void;
