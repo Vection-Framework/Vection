5595e02aa60faebd11b6eaa13ce90e16a545a29d
David Lung <git@davidlung.de>
2022-09-17 14:05:36 +0200

    BUGFIX Http #server-request-factory Fix return object when creating new server request.

--- src/Component/Http/Server/Message/Factory/ServerRequestFactory.php
+++ src/Component/Http/Server/Message/Factory/ServerRequestFactory.php
@@ -14,10 +14,11 @@ declare(strict_types=1);
 namespace Vection\Component\Http\Server\Message\Factory;
 
 use Psr\Http\Message\ServerRequestInterface;
+use Vection\Component\Http\Common\Factory\HeadersFactory;
 use Vection\Component\Http\Psr\Message\Factory\ServerRequestFactory as PsrServerRequestFactory;
 use Vection\Component\Http\Server\Factory\UriFactory;
-use Vection\Component\Http\Server\Message\ServerRequest;
 use Vection\Component\Http\Server\Environment;
+use Vection\Component\Http\Server\Message\ServerRequest;
 
 /**
  * Class ServerRequestFactory
@@ -33,18 +34,26 @@ class ServerRequestFactory extends PsrServerRequestFactory
      *
      * @return ServerRequest
      */
-    public function createServerRequestFromEnvironment(Environment $environment = null): ServerRequestInterface
+    public function createServerRequestFromEnvironment(Environment $environment = null): ServerRequest
     {
         if ( ! $environment ) {
             $environment = new Environment();
         }
 
         $uriFactory = new UriFactory();
+        $headersFactory = new HeadersFactory($environment);
 
-        return $this->createServerRequest(
-            $environment->getRequestMethod(),
+        return (new ServerRequest(
+            $method = strtoupper($environment->getRequestMethod()),
             $uriFactory->createUriFromEnvironment(),
-            $environment->toArray()
-        );
+            $headers = $headersFactory->createHeaders(),
+            explode('/', $environment->getServerProtocol())[1],
+            $environment)
+        )
+            ->withBody($stream = $this->streamFactory->createStreamFromFile('php://input'))
+            ->withParsedBody($this->parseBody($method, $stream, $headers))
+            ->withUploadedFiles($this->createUploadedFiles())
+            ->withQueryParams($_GET)
+            ->withCookieParams($_COOKIE);
     }
 }
