14826dfc1d9370379c344cba1623fcc45ecb69d0
David Brucksch <david.brucksch@appsdock.de>
2022-11-18 19:19:55 +0100

    NEW #common #yaml Added YAML class to parse and store YAML data.

---
+++ src/Component/Common/Yaml.php
@@ -0,0 +1,59 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Vection\Component\Common;
+
+use Vection\Component\Common\Exception\RuntimeException;
+
+/**
+ * @author BloodhunterD <bloodhunterd@bloodhunterd.com>
+ */
+class Yaml
+{
+    public static function parse(string $yaml, bool $immutable = false): VArray
+    {
+        if (!function_exists('yaml_parse')) {
+            throw new RuntimeException('Yaml::parse(yaml) requires php-yaml extension to be enabled.');
+        }
+
+        $data = yaml_parse($yaml);
+
+        if ($data === false) {
+            throw new RuntimeException('Malformed YAML');
+        }
+
+        return new VArray($data, $immutable);
+    }
+
+    public static function read(string $filePath): VArray
+    {
+        return VArray::parseFile($filePath);
+    }
+
+    /**
+     * @param VArray|array<mixed> $data
+     */
+    public static function encode(VArray|array $data): string
+    {
+        if (!function_exists('yaml_emit')) {
+            throw new RuntimeException('Yaml::encode(yaml) requires php-yaml extension to be enabled.');
+        }
+
+        return yaml_emit($data);
+    }
+
+    /**
+     * @param VArray|array<mixed>|string $data
+     */
+    public static function toFile(string $filePath, VArray|array|string $data): void
+    {
+        if (!function_exists('yaml_emit_file')) {
+            throw new RuntimeException('Yaml::toFile(yaml) requires php-yaml extension to be enabled.');
+        }
+
+        if (!yaml_emit_file($filePath, $data)) {
+            throw new RuntimeException('Failed to save YAML data in file.');
+        }
+    }
+}
